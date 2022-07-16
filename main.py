a6f7f58cde99984ed6cfc7f980855cd06363fa30
ikuma-hiroyuki <chemicalbondo@gmail.com>
2022-07-16 15:51:11 +0900

    - マジックナンバーを定数に - turtleインスタンスをcolorsの長さ分に

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from turtle import Screen, Turtle
 import random
 
+TURTLE_SIZE = 40
+SCREEN_WIDTH = 500
+SCREEN_EDGE = (SCREEN_WIDTH - (TURTLE_SIZE / 2)) / 2
+
 screen = Screen()
-screen.setup(height=400, width=500)
+screen.setup(height=400, width=SCREEN_WIDTH)
 user_bet = screen.textinput(title="Make your bet.", prompt="Witch turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 all_turtles = []
 
 y_pos = 90
-for turtle_index in range(6):
+for turtle_index in range(len(colors)):
     new_turtle = Turtle("turtle")
     new_turtle.color(colors[turtle_index])
     new_turtle.penup()
-    new_turtle.goto(x=-230, y=y_pos)
+    new_turtle.goto(x=SCREEN_EDGE * -1, y=y_pos)
     y_pos -= 30
     all_turtles.append(new_turtle)
 
@@ -25,7 +29,7 @@ while is_race_on:
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
 
-        if turtle.xcor() >= 230:
+        if turtle.xcor() >= SCREEN_EDGE:
             winning_color = turtle.pencolor()
             is_race_on = False
 
